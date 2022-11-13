9fa21c10d02e6b668028fb95b231045d12cf2c69
Lukaz <lukaz.martin@bling.eu>
2022-11-13 18:19:46 +0100

    filter information, safe csv

--- source/scraper.py
+++ source/scraper.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
+from pathlib import Path
+import csv
+
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 
 class Scraper:
@@ -15,6 +18,9 @@ class Scraper:
         self.linksToExplore = []
         self.next_url = None
         self.explore = True
+        self.csv = csv.writer(open(BASE_DIR / 'dataset/engelvoelkers_houses_bcn.csv', 'w'))
+        self.csv.writerow(['title', 'location', 'location_status', 'status', 'year', 'area', 'bathrooms', 'bedrooms',
+                           'heating_type', 'energy_class', 'price'])
 
     def __get_page(self, user_agent=None):
 
@@ -30,7 +36,7 @@ class Scraper:
         self.page = page_
 
     def __get_soup(self):
-        soup_ = BeautifulSoup(self.page.content)
+        soup_ = BeautifulSoup(self.page.content, features="html.parser")
         self.soup = soup_
 
     def __get_links(self):
@@ -52,60 +58,96 @@ class Scraper:
         index = self.url.index(self.index_parameter)
         self.current_index += self.pagination_index
 
-        if self.next_url is None:  # Hay 2 anuncios en lugar de casas en la primera pagina
+        if self.next_url is None:  # There are 2 advertisements on the first page instead of houses
             self.current_index -= 2
 
         self.next_url = self.url[:index + len(self.index_parameter) + 1] + str(self.current_index)
         self.next_url += self.url[index + len(self.index_parameter) + 2:]
 
-    def __scrape_page(self, user_agent=None):
-        # Inicializamos un dataframe
-        df = pd.DataFrame()
-        for k in self.linksToExplore:
-            # Creamos un diccionario para cada una de las páginas.
-            dic_ = dict()
-            subpage_ = requests.get(k, headers={"User-Agent": user_agent})
-            subsoup_ = BeautifulSoup(subpage_.content)
+    def __scrape_sub_pages(self, user_agent=None):
 
-            dic_["Título"] = [subsoup_.find('h1', class_='ev-exposee-title ev-exposee-headline').text]
-            dic_["Descripción"] = [subsoup_.find('p', class_='ev-exposee-text').text]
-            dic_["Localización"] = [subsoup_.find('div', class_='ev-exposee-content ev-exposee-subtitle').text]
-            fact_titles = subsoup_.find_all('div', class_='ev-key-fact-title')
-            fact_values = subsoup_.find_all('div', class_='ev-key-fact-value')
-            detail_facts = subsoup_.find_all('li', class_='ev-exposee-detail-fact')
+        for link in self.linksToExplore:
 
-            for i in range(0, len(fact_titles)):
-                dic_[fact_titles[i].text] = [fact_values[i].text]
+            try:
+                subpage_ = requests.get(link, headers={"User-Agent": user_agent})
+                soup_ = BeautifulSoup(subpage_.content, features="html.parser")
 
-            for i in detail_facts:
-                conq = []
-                if i.find('label') is not None:
-                    if len(i.find_all('span')) > 1:
-                        for j in range(0, len(i.find_all('span'))):
-                            conq.append(i.find_all('span')[j].text)
-                        dic_[i.find('label').text] = ['/'.join(conq)]
-                    else:
-                        dic_[i.find('label').text] = [i.find('span').text]
+                title = soup_.find('h1', class_='ev-exposee-title ev-exposee-headline').text
+                location = soup_.find('div', class_='ev-exposee-content ev-exposee-subtitle').text
+                fact_titles = [title.text for title in soup_.find_all('div', class_='ev-key-fact-title')]
+                fact_values = [value.text for value in soup_.find_all('div', class_='ev-key-fact-value')]
+                detail_facts = [detail.text for detail in soup_.find_all('li', class_='ev-exposee-detail-fact')]
 
-            if df.empty:
-                df = pd.DataFrame(dic_)
-            else:
-                df_aux = pd.DataFrame(dic_)
-                df = pd.concat([df, df_aux], ignore_index=True)
+                bedrooms, bathrooms, area, price = None, None, None, None
+                for i in range(len(fact_titles)):
+                    category = fact_titles[i].replace(" ", "")
+                    if category == 'Dormitorios' or category == 'Cuartos':
+                        bedrooms = fact_values[i]
+                    elif category == 'Baños':
+                        bathrooms = fact_values[i]
+                    elif category == 'Superficiehabitableaprox.' or category == 'Superficieconstruidaaprox.':
+                        area = fact_values[i]
+                    elif category == 'Precio':
+                        price = fact_values[i]
+
+                try:
+                    index = location.index("Barcelona")
+                    location = location[index+11:]
+                except ValueError:
+                    location = None
+
+                year, status, location_status, energy_class, heating_type = None, None, None, None, None
+                for detail in detail_facts:
+                    if 'Año de construcción' in detail:
+                        year = detail[detail.index('Año de construcción')+20:].replace(" ", "")
+                    elif 'Estado' in detail:
+                        status = detail[detail.index('Estado')+7:]
+                    elif 'Ubicación' in detail:
+                        location_status = detail[detail.index('Ubicación')+10:]
+                    elif 'Clase de eficiencia energética' in detail:
+                        energy_class = detail[detail.index('Clase de eficiencia energética')+31:]
+                    elif 'Tipo de calefacción' in detail:
+                        heating_type = detail[detail.index('Tipo de calefacción')+21:]
+
+                # Now we have extracted the values we want
+                self.__preprocess_and_safe(title, location, bedrooms, bathrooms, area, price, year, status, location_status, energy_class, heating_type)
+
+            except:
+                pass
+
+    def __preprocess_and_safe(self, title, location, bedrooms, bathrooms, area, price, year, status, location_status,
+                              energy_class, heating_type):
+
+        # We remove the commas that can appear
+        title = title[1:].replace(",", " ") if title is not None else None
+        location = location.replace(",", " ") if location is not None else None
+        location_status = location_status.replace(",", " ").replace(" ", "") if location_status is not None else None
+        status = status.replace(",", " ").replace(" ", "") if status is not None else None
+        year = year.replace(",", "").replace(" ", "") if year is not None else None
+        area = area.replace(",", "").replace(" ", "") if area is not None else None
+        bathrooms = bathrooms.replace(",", "").replace(" ", "") if bathrooms is not None else None
+        bedrooms = bedrooms.replace(",", "").replace(" ", "") if bedrooms is not None else None
+        heating_type = heating_type.replace(",", " ").replace(" ", "") if heating_type is not None else None
+        energy_class = energy_class.replace(",", "").replace(" ", "") if energy_class is not None else None
+        price = price.replace(",", "").replace(" ", "") if price is not None else None
+
+        # And safe it in the csv
+        self.csv.writerow([title, location, location_status, status, year, area, bathrooms, bedrooms, heating_type,
+                           energy_class, price])
 
     def scrape(self):
+
+        # First we search for all the links we want to explore
         while self.explore:
             self.__get_page()
             self.__get_soup()
             self.__get_links()
             self.__filter_links()
             self.__get_next_link()
-        self.__scrape_page()
-        #  TODO: Hasta aquí he recorrido todas las paginas guardando los links importantes en self.linksToExplore
-        # Lo que deberíamos hacer ahora es:
-        # 1. Recorrer todos los links guardados e buscar en cada uno la información más relevante
-        #   - Titulo, Descripcion, numero dormitorios, numero baños, superficie, precio, calle..
-        # 2. Guardar esta información como una nueva fila en un CSV que guardaremos en /dataset
+
+        # Now we scrape those links, search the relevant information and save it in a CSV file
+        self.__scrape_sub_pages()
+
 
 
 if __name__ == '__main__':
